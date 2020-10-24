7d28ec4e74644949cc06c7c5e81d3198c90dc0eb
amazingProj <68805670+amazingProj@users.noreply.github.com>
2020-10-25 00:26:18 +0300

    Delete Debts_rights.kt

--- src/Debts_rights.kt
+++
@@ -1,8 +0,0 @@
-/**
- * interface represents debts and rights of a single member at the community
- */
-interface Debts_rights {
-    fun charge():Float// calc of tax charge
-    fun deserve_money():Float//maximum received money
-    fun volunteer():Int//number of hours of volunteering
-}
