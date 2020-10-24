c84ad8f29e260deb87ba2c55eb2d590a49a2c125
amazingProj <68805670+amazingProj@users.noreply.github.com>
2020-10-25 00:23:19 +0300

    Update Community.kt

--- src/Community.kt
+++ src/Community.kt
@@ -1,4 +1,3 @@
-/*
 /**
  * Community member class for the community that includes members:
  * married and unmarried
@@ -7,11 +6,20 @@
  *
  */
 class Community {
+
+    /******************properties **********/
     /**
      * the class community has a list of her community members
      */
-    public var communityMembers: MutableList<Community_member> ?= (null)
-        get() = communityMembers
+    public var communityMembers: MutableList<CommunityMember> = mutableListOf()
+
+    // ?= (null)
+    //  get() = communityMembers
+    operator fun iterator(): Iterator<CommunityMember> {             // 1
+        return communityMembers.iterator()                           // 2
+    }
+
+    /*************** constuctor ******************/
 
     /**
      * construct a community by firstly construct a list of members
@@ -19,13 +27,13 @@ class Community {
      * @return void constructing a community by values
      * @suppress frist puting new argument to a null list
      */
-    constructor(vararg values: Community_member){
-        if(!(communityMembers.isNullOrEmpty()))
+    constructor(vararg values: CommunityMember) {
+        if (!(communityMembers.isNullOrEmpty()))
             communityMembers?.clear()
-       //communityMembers.addAll(values)
+        communityMembers.addAll(values)
     }
 
-
+    /******************* function *********************/
 
     /**
      *  add few community members
@@ -33,33 +41,21 @@ class Community {
      *  @return void just doing manipulation on community's list
      *  @suppress frist puting new argument to a null list
      */
-    fun addCommunityMembers(vararg values: Community_member) {
-        for (v: Community_member in values) {
-            addANewMember(v)
-        }
-        //communityMembers.addAll(values)
+    fun addCommunityMembers(vararg values: CommunityMember) {
+        communityMembers.addAll(values)
     }
 
     /**
-     * add a new member
-     * @param s member to add
-     * @return void adding a new member
-     * @suppress frist puting new argument to a null list
+     * function calculate the total amount of charging taxes
+     * @return total - amount of money of taxes
      */
-    fun addANewMember(s: Community_member) {
-       // communityMembers.add(s)
-    }
-
-    /**
-     *
-     */
-    fun total_taxes_amount():Float {
-        var total:Float = 0f
-        for (v: Community_member in communityMembers) {
-                  total += v.charge()
+    fun totalTaxesAmount(): Float {
+        var total: Float = 0f
+        for (v: CommunityMember in communityMembers) {
+            total += v.charge()
         }
-       return total
-        return  0f
+        return total
+        return 0f
     }
 
     /**
@@ -70,36 +66,44 @@ class Community {
      * @param mem - represents the community member which pull the request
      * @return money that he/she could get
      */
-    fun Gemach_approval_request(mem:Community_member) : Int {
-        if (!(mem.attempts == 0)){
-        val calcMoney: (Float) -> Float = { a: Float -> a * mem.deserve_money().toFloat() }
-        var mo: Float = 0f //Init of var
-        if (mem.payment < 8000) {
-            mo = calcMoney(1f)
-        } else
-            if ((mem.works > 0) and (mem.Torah > 30)) {
+    fun GemachApprovalRequest(mem: CommunityMember): Int {
+        if (mem.attempts == 0)
+            return 0; //if there is no attempts available
+
+        val calcMoney = { a: Float -> a * mem.desrveMoneyfromGemach().toFloat() }
+        var mo = 0f //Init of var
+        when {
+            mem.payment < 8000 -> {
+                mo = calcMoney(1f)
+            }
+            (mem.works > 0) and (mem.Torah > 30) -> {
                 mo = calcMoney(0.7f)
-            } else mo = calcMoney(0.3f)
+            }
+            else -> mo = calcMoney(0.3f)
+        }
         mem.attempts--
         return mo.toInt()
     }
-    return 0; //if there is no attempts avaible
-    }
 
     /**
-     *
+     * each community member has a contribution this function
+     *@return a sorted list -  types by this order  the first is spiritual contribution members and then
+     * practical contribution members  and musician contribution members
      */
-    fun sorted_hours_of_volunteering() : MutableList<Community_member>{
-        var volunteers: MutableList<Community_member> = mutableListOf()
-        var lambda = {_str:String ->  for (v in communityMembers){
-            if(v.str == _str)
-                volunteers.add(v)
-        }}
-        lambda("spirit")
-        lambda("practical")
-        lambda("musician")
-        return  volunteers
+    fun sortedHoursofVolunteering(): MutableList<CommunityMember> {
+        var volunteers: MutableList<CommunityMember> = mutableListOf()
+        var lambda: (srt: String) -> Unit = { str: String ->
+            for (v in communityMembers) {
+                if (v.getVolTypes().toString() == v.getVolTypesbyName(str).toString()) {
+                    volunteers.add(v)
+                }
+            }
+        }
+        lambda("SPIRIT")
+        lambda("PRACTICAL")
+        lambda("MUSICIAN")
+        return volunteers
     }
-
 }
-*/
+
+
